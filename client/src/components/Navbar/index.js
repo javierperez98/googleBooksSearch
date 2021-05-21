324b250db2d9968696abb939e8f858ea994d5737
Javier Perez <javier21perez98@gmail.com>
2021-05-20 22:19:02 -0700

    created navbar componet

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-dark bg-dark p-3">
+		<nav className="navbar navbar-expand navbar-dark bg-dark p-3">
 			<div className="container-fluid">
-				<Link className="navbar-brand fs-2" to="/">
-					Books Directory
+				<Link className="navbar-brand" to="/">
+					Home
 				</Link>
 				<ul className="navbar-nav me-auto">
 					<li className="nav-item px-2">
-						<Link className="nav-link" aria-current="page" to="/search">
+						<Link className="nav-link py-0" aria-current="page" to="/search">
 							Search
 						</Link>
 					</li>
 					<li className="nav-item px-2">
-						<Link className="nav-link" to="">
+						<Link className="nav-link py-0" to="">
 							Saved
 						</Link>
 					</li>
