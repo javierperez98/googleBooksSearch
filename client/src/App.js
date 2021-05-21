25539646c3adb8cadb7f9c2ceb83dfb1109c97ca
Javier Perez <javier21perez98@gmail.com>
2021-05-20 22:16:39 -0700

    created route for search and homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import Search from "./pages/Search";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path={["/", "/books"]}>
-            <Books />
-          </Route>
-          <Route exact path="/books/:id">
-            <Detail />
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div>
+				<Navbar />
+				<Switch>
+					<Route exact path={["/", "/search"]}>
+						<Search />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
