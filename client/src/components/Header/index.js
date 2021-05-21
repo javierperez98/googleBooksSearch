bbf0bac80225d285fb678858c14041e50d5c5ae6
Javier Perez <javier21perez98@gmail.com>
2021-05-20 22:18:30 -0700

    created header component

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+	return (
+		<div className="container card bg-dark text-white p-3">
+			<h1 className="card-title">React Google Books Search</h1>
+			<p className="card-text">
+				Where you can Search for and Save Books of interest!
+			</p>
+		</div>
+	);
+}
+
+export default Header;
