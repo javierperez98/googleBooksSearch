b9629ea951196a39ac919b08f7ce311a3ef0a931
Javier Perez <javier21perez98@gmail.com>
2021-05-22 10:52:26 -0700

    updadted name and added getbooks route

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
 const API = {
-	getBooks: function (title) {
+	searchBooks: function (title) {
 		return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + title);
 	},
 
 	saveBook: function (bookData) {
 		return axios.post("/api/books", bookData);
+	},
+
+	getBooks: function () {
+		axios.get("/api/books");
 	},
 
 	deleteBook: function (id) {
