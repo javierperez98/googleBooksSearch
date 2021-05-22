06e5fcbfda5cc08f042691e4be75f60a51dd0988
Javier Perez <javier21perez98@gmail.com>
2021-05-21 19:13:06 -0700

    removed jsx and created seperate file for components

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import API from "../utils/API";
 import Header from "../components/Header";
+import SearchBar from "../components/SearchBar";
+import Rows from "../components/Rows";
 
 function Search() {
 	const [state, setState] = useState({
@@ -17,61 +19,18 @@ function Search() {
 	async function findbooks(e) {
 		e.preventDefault();
 		const list = await API.getBooks(state.search.trim());
-		console.log(list.data.items);
 		setState({ books: list.data.items, search: "" });
 	}
+
 	return (
 		<div className="container mt-5">
 			<Header />
-			<div className="container mt-5 border border-5 border-dark">
-				<h1>Search for books:</h1>
-				<form className="row">
-					<div className="col-8">
-						<input
-							value={state.search}
-							onChange={handleInput}
-							className="form-control form-control"
-							type="text"
-							placeholder="Title"
-						/>
-					</div>
-					<div className="col-4">
-						<button
-							type="sumbit"
-							onClick={findbooks}
-							className="btn btn-primary mb-3 col-12"
-						>
-							Search
-						</button>
-					</div>
-				</form>
-			</div>
-			<div className="container my-5 pt-3 border border-5 border-dark">
-				<h1>Results</h1>
-				{state.books.map((book) => {
-					return (
-						<div key={book.id} className="card mb-3">
-							<div className="row g-0">
-								<div className="col-md-auto">
-									<img
-										className="m-3 img-fluid"
-										src={book.volumeInfo.imageLinks.thumbnail}
-										alt={book.volumeInfo.title}
-									/>
-								</div>
-								<div className="col-md-8">
-									<div className="card-body">
-										<h5 className="card-title">{book.volumeInfo.title}</h5>
-										<p>{`Written By: ${book.volumeInfo.authors}`}</p>
-										<p className="card-text">{book.volumeInfo.subtitle}</p>
-										<p>{book.volumeInfo.description}</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					);
-				})}
-			</div>
+			<SearchBar
+				search={state.search}
+				handleInput={handleInput}
+				findbooks={findbooks}
+			/>
+			<Rows array={state.books} />
 		</div>
 	);
 }
