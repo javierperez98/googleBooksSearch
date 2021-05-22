0ec0e4bc3617792fdd5e79d58375897aefa4b97e
Javier Perez <javier21perez98@gmail.com>
2021-05-22 10:49:54 -0700

    links for pages added

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -10,12 +10,12 @@ function Navbar() {
 				</Link>
 				<ul className="navbar-nav me-auto">
 					<li className="nav-item px-2">
-						<Link className="nav-link py-0" aria-current="page" to="/search">
+						<Link className="nav-link py-0" aria-current="page" to="/">
 							Search
 						</Link>
 					</li>
 					<li className="nav-item px-2">
-						<Link className="nav-link py-0" to="">
+						<Link className="nav-link py-0" to="/saved">
 							Saved
 						</Link>
 					</li>
