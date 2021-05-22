0c1d36b53319cd6a4f3225728cfb7c54ec2226e2
Javier Perez <javier21perez98@gmail.com>
2021-05-22 10:52:57 -0700

    updated api routes

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
 
-// Matches with "/api/books"
-router.route("/").get(booksController.findAll).post(booksController.create);
+router.route("/").get(booksController.findAll);
 
-// Matches with "/api/books/:id"
+router.post(booksController.create);
+
 router.delete(booksController.remove);
 
 module.exports = router;
